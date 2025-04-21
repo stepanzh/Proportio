53243f69c98ae399b89112d56c1f59a57aef6830
stepanzh <stepanzh@gmail.com>
2025-04-21 20:37:38 +0300

    feat(landing): draft of landing page, few basic components
    Related #87

---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="card">
+        <slot />
+    </div>
+</template>
+
+<script setup />
+
+<style>
+.card {
+    padding: 48px 18px;
+    background-color: white;
+    box-shadow: var(--elevation-1);
+}
+</style>
---
+++ src/components/CulinaryUseCase.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="culinary-use-case">
+        <h4 class="sh3">{{ emoji }} {{ title }}</h4>
+        <p class="stxt culinary-use-case__description">{{ description }}</p>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+    emoji: { type: String, required: true },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+})
+</script>
+
+<style>
+.culinary-use-case {
+    display: flex;
+    flex-direction: column;
+    gap: 6px;
+}
+
+.culinary-use-case__description { color: var(--shuttle-gray-600); }
+
+</style>
