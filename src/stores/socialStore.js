513b41a8eacae3920eca1d9e3c8df86e13bd26a5
stepanzh <stepanzh@gmail.com>
2025-05-10 13:08:06 +0300

    feat(landing): add feedback survey and general feedback form
    Closes #94

--- src/stores/socialStore.js
+++ src/stores/socialStore.js
@@ -4,8 +4,12 @@ export const useSocialStore = defineStore('social', () => {
     const telegramChannel = new URL("https://t.me/proportioapp")
     const vkChannel = new URL("https://vk.com/proportioapp")
     const stepanzhBlog = new URL("https://stepanzh.github.io")
+    const feedbackSurvey = new URL("https://forms.yandex.ru/u/681e21d8d04688c05974798f/")
+    const feedbackFreeForm = new URL("https://forms.yandex.ru/u/681f2013eb61467af495de02/")
 
     return {
+        feedbackFreeForm,
+        feedbackSurvey,
         telegramChannel,
         vkChannel,
         stepanzhBlog,
