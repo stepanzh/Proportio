966c2e518fdb77cc4f51b0b1c36733ceb782a94b
stepanzh <stepanzh@gmail.com>
2026-08-08 19:02:02 +0300

    fix: rustore button as image plus link

--- src/ui/PRuStoreButton.vue
+++ src/ui/PRuStoreButton.vue
@@ -1,19 +1,24 @@
 <template>
-    <div class="btn-rustore">
-        <iframe
-            src="https://www.rustore.ru/external/simple-selection/buttons?theme=coloredLight&orientation=default&stores=rustore%3Ahttps%3A%2F%2Fwww.rustore.ru%2Fcatalog%2Fapp%2Fru.stepanzh.proportio%3Futm_source%3Davailable_in_rustore%26utm_medium%3Dru.stepanzh.proportio%26rsm%3D1%26mt_link_id%3Diios36%26mt_sub1%3Dru.stepanzh.proportio"
-            frameborder="0"
-            width="100%"
-            height="40px">
-        </iframe>
-    </div>
+    <a :href="rustoreHref" class="btn-rustore">
+        <img :src="RustoreDownloadLight" class=".img-rustore-download" />
+    </a>
 </template>
+
+<script setup>
+import RustoreDownloadLight from '@/assets/logo/rustore-download-light.svg'
+import { useSocialStore } from '@/stores/socialStore'
+
+const rustoreHref = (useSocialStore()).rustoreApp
+</script>
 
 <style>
 .btn-rustore {
     display: inline-block;
-    overflow: hidden;
-    max-width: 108px;
-    max-height: 40px;
+    height: 40px;
+}
+
+.img-rustore-download {
+    width: 111px;
+    height: 40px
 }
 </style>
